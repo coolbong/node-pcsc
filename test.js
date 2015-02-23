6ab07fea44045c29ada11926982388e6e070f398
coolbong <coolbong.works@gmail.com>
2015-02-23 11:42:11 +0900

    change select aid

--- test.js
+++ test.js
@@ -16,7 +16,8 @@ pcsc.select_reader(readers[0]);
 
 var atr = pcsc.connect();
 console.log('atr : ' + atr.toString('hex').toUpperCase());
-input = new Buffer('00A4040008A000000151000000', 'hex');
+input = new Buffer('00A4040008A000000003000000', 'hex');
+//input = new Buffer('00A4040008A000000151000000', 'hex');
 
 result = pcsc.sendapdu(input);
 console.log(result);
